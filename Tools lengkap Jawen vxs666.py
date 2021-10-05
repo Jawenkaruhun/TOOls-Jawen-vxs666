b968a1d2d6938e7098524c94960e35e9ebf4ba2f
Jawenkaruhun <91988917+Jawenkaruhun@users.noreply.github.com>
2021-10-06 04:49:04 +0700

    Add files via upload

---
+++ Tools lengkap Jawen vxs666.py
@@ -0,0 +1,663 @@
+#! /sistem/xbin/bash
+jernih
+biru= ' \033[34;1m '
+hijau= ' \033[32;1m '                                        
+ungu= ' \033[35;1m '
+cyan= ' \033[36;1m '
+merah= ' \033[31;1m '
+putih= ' \033[37;1m '                                           
+kuning= ' \033[33;1m '
+tidur 1
+toilet -f miring -F gay " T00Ls-Jawen vxs666 "  | lolcat
+echo  " \033[35;1m "
+kal
+tanggal
+gema  " "      
+echo  " \033[32;1mAuthor : Jawen vxs666 " 
+echo  " \033[35;1mTeam : Alone Black Hat "
+echo  " \033[36;1mIndonesia Black Hat "
+echo  " \033[37;1mJawaTengah Blackhat  "
+echo  " \033[33;1mKontak Saya : 083861278853"
+echo  " \033[33;1mDarknessCyberTeam "  " \033[32;1m& "  " \033[36;1mIndonesia Black Hat "
+echo  " \033[34;1m[============================[> "
+tidur 1
+gema  " "
+echo  " \033[32;1mSilahkan Pilih Tools Yang mau Kamu pake :)?: "
+echo  " \033[36;1m "
+echo  " [============================================== [> "
+echo  " 1.> Spam Sms Tokopedia "
+echo  " [============================================== ]> "
+echo  " 2.> Spam Sms Telkomnyet "
+echo  " [============================================== ]> "
+echo  " 3.> Spam Sms Matahari Mall "
+echo  " [============================================== ]> "
+echo  " 4.> Spam Sms HooqTV "
+echo  " [============================================== ]> "
+echo  " 5.> Spam Sms KFC "
+echo  " [============================================== ]> "
+echo  " 6.> Spam Sms PHD "
+echo  " [============================================== ]> "
+echo  " 7.> Spam Sms Whiskas "
+echo  " [============================================== ]> "
+echo  " 8.> Spam Sms ZiPAY "
+echo  " [============================================== ]> "
+echo  " 9.> Nyari CC buat Carding "
+echo  " [============================================== ]> "
+echo  " 10.> Spam Akun Gmail "
+echo  " [============================================== ]> "
+echo  " 11.> Pencarian Whois "
+echo  " [============================================== ]> "
+echo  " 12.> Unggah Admin Finder "
+echo  " [============================================== ]> "
+echo  " 13.> Pembuat Skrip Deface "
+echo  " [============================================== ]> "
+echo  " 14.> ReConDog Buat Pengumpulan Informasi "
+echo  " [============================================== ]> "
+echo  " 15.> Pencari Login Admin "
+echo  " [============================================== ]> "
+echo  " 16.> Pembuat Virus VbugMaker "
+echo  " [============================================== ]> "
+echo  " 17.> dowload script deface orang "
+echo  " [============================================== ]> "
+echo  " 18.> Suka Otomatis Facebook "
+echo  " [============================================== ]> "
+echo  " 19.> Facebook Auto_Reaction "
+echo  " [============================================== ]> "
+echo  " 20.> Bot Komentar Facebook "
+echo  " [============================================== ]> "
+echo  " 21.> Instal txttool "
+echo  " [============================================== ]> "
+echo  " 22.> instal sqlscan "
+echo  " [============================================== ]> "
+echo  " 23.> instal hash-buster "
+echo  " [============================================== ]> "
+echo  " 24.> Instal Red_Hawk "
+echo  " [============================================== ]> "
+echo  " 25.> Instal Torshammer buat situs web DDOS "
+echo  " [============================================== ]>"
+echo  " 26.> Instal LITEDDOS buat website DDOS "
+echo  " [============================================== ]>"
+echo  " 27.> Instal Kerangka Pemburu "
+echo  " [============================================== ]>"
+echo  " 28.> Instal Sqlmap Buat Situs Inject "
+echo  " [============================================== ]>"
+echo  " 29.> Instal 4wsectools "
+echo  " [============================================== ]>"
+echo  " 30.> Install Lazymux Buat Install Tools "
+echo  " [============================================== ]>"
+echo  " 31.> Mempercepat / Menstabilkan Jaringan/sinyal "
+echo  " [============================================== ]>"
+gema  " 32.> Mempercepat / mengejar jaringan 2 "
+gema " [============================================== ]> "
+gema  " 33.> Instal BinGoo "
+gema " [============================================== ]> "
+gema  " 34.> Bot Penuh Facebook "
+gema " [============================================== ]> "
+gema  " 35.> Instal Termux-A "
+gema " [============================================== ]> "
+gema  " 36.> Instal Alat GadoGado "
+gema " [============================================== ]> "
+gema  " 37.> Instal Lazysqlmap "
+gema " [============================================== ]> "
+gema  " 38.> Instal tools diejoubu "
+gema " [============================================== ]> "
+gema  " 39.> Install weeman Buat Phising "
+gema " [============================================== ]> "
+gema  " 40.> Instal iesDEFACE buat Deface Poc Webdav "
+gema " [============================================== ]> "
+gema  " 41.> install errorcybertools "
+gema  " [============================================== ]> "
+gema  " 42.> install A-Rat untuk Retas Android "
+gema  " [============================================== ]> "
+gema  " 43.> Pindai-Website-Admin-Login "
+gema  " [============================================== ]> "
+gema  " 44.> Metode DEFACE Webdav "
+gema  " [============================================== ]> "
+gema  " 45.> Alat Katoolin "
+gema  " [============================================== ]> "
+gema  " 46.> Main Tebak2 kan:v biar ga bt:v "
+gema  " [============================================== ]> "
+gema  " 47.> Brute DEFACE webdav "
+gema  " [============================================== ]> "
+gema  " 48.> Browsing Di Termux "
+gema  " [============================================== ]> "
+gema  " 49.> Dengerin Lagu Youtube di Termux "
+gema  " [============================================== ]> "
+gema  " 50.> Tebak Tebak Kan Soal Cinta Tod " 
+echo  " [============================================== ]>"
+echo  " 51.> Nuyul Bitcoin "
+echo  " [============================================== ]>"
+echo  " 52.> Instal MetaSploit "
+echo  " [============================================== ]>"
+echo  " 53.> MultiBrute Force Facebook Buat Hack Facebook "
+echo  " [============================================== ]>"
+echo  " 54.> instal WAScan "
+echo  " [============================================== ]>"
+echo  " 55.> install MultiSpam [Sms] "
+echo  " [============================================== ]>"
+echo  " 56.> Spam Sms Jd.id "
+echo  " [============================================== ]>"
+echo  " 57.> DDOS xerxes "
+echo  " [============================================== ]>"
+echo  " 58.> Lokasi IPGeo Buat Lacak IP "
+echo  " [============================================== ]>"
+echo  " 59.> Instal viSQL "
+echo  " [============================================== ]> "
+echo  " 60.> Instal Kawai-Botnet Buat DDOS "
+echo  " [============================================== ]> "
+echo  " 61.> Instal OWScan "
+echo  " [============================================== ]>"
+echo  " 62.> Instal AstraNmap "
+echo  " [============================================== ]>"
+echo  " 63.> Instal Auxscan2.0 "
+echo  " [============================================== ]>"
+echo  " 64.> Instal Black-Hydra "
+echo  " [============================================== ]>"
+echo  " 65.> Instal Ipddos "
+echo  " [============================================== ]>"
+echo  " 66.> Instal hasher "
+echo  " [============================================== ]>"
+echo  " 67.> Install PhisingGame Buat Hack ML & COC "
+echo  " [============================================== ]>"
+echo  " 68.> Kumpulan Tutorial Termux "
+echo  " [============================================== ]> "
+echo  " 69.> Chating Di Termux "
+echo  " [============================================== ]> "
+echo  " 70.> Cari Panel Login Admin Pakai Breacer "
+echo  " [============================================== ]>"
+echo  " 71.> Install SocialFish Buat Phising "
+echo  " [============================================== ]>"
+echo  " 72.> Install MoonBuggy Buat Main Game "
+echo  " [============================================== ]>"
+echo  " 73.> Install Cupp Buat Worldlist Tertarget "
+echo  " [============================================== ]>"
+echo  " 74.> Instal Hydra "
+echo  " [============================================== ]>"
+echo  " 75.> Instal md5-crack "
+echo  " [============================================== ]>"
+echo  " 76.> Instal LITESPAM Buat Spam "
+echo  " [============================================== ]>"
+echo  " 77.> Mulung Bitcoin "
+echo  " [============================================== ]> "
+echo  " 78.> Instal Ko-Dork "
+echo  " [============================================== ]> "
+echo  " 79.> Lihat Spesifikasi Ponsel "
+echo  " [============================================== ]>"
+echo  " 80.> instal lhst "
+echo  " [============================================== ]>"
+echo  " 0.> keluar "
+echo  " [============================================== ]>"
+baca -p " root@T00Ls-Jawen vxs666]~# " bang
+
+jika [ $bro  = 1 ] || [ $kakak  = 1 ]
+kemudian
+jernih
+figlet " Jawen vxs666 "  | lolcat
+CDmodul 
+php tp.php
+fi
+
+jika
+[ $bro  = 2 ] || [ $kakak  = 2 ]
+kemudian
+jernih
+toilet " Jawen vxs666 "
+modul cd
+php t.php
+fi
+
+jika [ $bro  = 3 ] || [ $kakak  = 3 ]
+kemudian
+figlet " Jawen vxs666 "
+CDmodul 
+php mataharimall.php
+fi
+
+jika [ $bro  = 4 ] || [ $kakak  = 4 ]
+kemudian
+jernih
+toilet " Jawen vxs666 "
+CDmodul 
+php hooq.php
+fi
+
+jika [ $bro  = 5 ] || [ $kakak  = 5 ]
+kemudian
+jernih
+toilet -f miring --gay " Jawen vxs666 "
+CDmodul 
+php kfc.php
+fi
+
+jika [ $bro  = 6 ] || [ $kakak  = 6 ]
+kemudian
+jernih
+toilet -f mono12 -F gay " Jawen vxs666 "
+modul cd
+php phd.php
+fi
+
+jika [ $bro  = 7 ] || [ $kakak  = 7 ]
+kemudian
+jernih
+toilet " Jawen vxs666 "
+modul cd
+php whiskas.php
+fi
+
+jika [ $bro  = 8 ] || [ $kakak  = 8 ]
+kemudian
+jernih
+figlet " Jawen vxs666 "
+modul cd
+php zipay.php
+fi
+
+jika [ $bro  = 9 ] || [ $kakak  = 9 ]
+kemudian
+jernih
+toilet " Jawen vxs666 "
+modul cd
+kunci php.php
+fi
+
+jika [ $bro  = 10 ] || [ $kakak  = 10 ]
+kemudian
+jernih
+toilet " Jawen vxs666 "
+modul cd
+python2 SpamMail.py
+fi
+
+jika [ $bro  = 11 ] || [ $kakak  = 11 ]
+kemudian
+jernih
+toilet " Jawen vxs666 "
+modul cd
+python2 whs.py
+fi
+
+jika [ $bro  = 12 ] || [ $kakak  = 12 ]
+kemudian
+jernih
+toilet " Jawen vxs666 "
+modul cd
+python2 upf.py
+fi
+
+jika [ $bro  = 13 ] || [ $kakak  = 13 ]
+kemudian
+jernih
+toilet " Jawen vxs666 "
+modul cd
+python2 create.py
+fi
+
+jika [ $bro  = 14 ] || [ $kakak  = 14 ]
+kemudian
+jernih
+toilet " Jawen vxs666 "
+modul cd
+python2 dog.py
+fi
+
+jika [ $bro  = 15 ] || [ $kakak  = 15 ]
+kemudian
+jernih
+toilet " Jawen vxs666 "
+modul cd
+python2 gam.py
+fi
+
+jika [ $bro  = 16 ] || [ $kakak  = 16 ]
+kemudian
+jernih
+toilet " Jawen vxs666 "
+modul cd
+python2 anvima.py
+fi
+
+jika [ $bro  = 17 ] || [ $kakak  = 17 ]
+kemudian
+jernih
+toilet " Jawen vxs666 "
+modul cd
+python2 htmd.py
+fi
+
+jika [ $bro  = 18 ] || [ $kakak  = 18 ]
+kemudian
+jernih
+toilet " Jawen vxs666 "
+modul cd
+php autolike.php
+fi
+
+jika [ $bro  = 19 ] || [ $kakak  = 19 ]
+kemudian
+jernih
+toilet " Jawen vxs666 "
+modul cd
+python2 auto_reaction.py
+fi
+
+jika [ $bro  = 20 ] || [ kakak = 20 ]
+kemudian
+jernih
+figlet " Jawen vxs666 "  | lolcat
+modul cd
+python2 botkomen.py
+fi
+
+jika [ $bro  = 21 ] || [ $kakak  = 21 ]
+kemudian
+jernih
+figlet " Jawen vxs666 "  | lolcat
+git clone https://github.com/kuburan/txtool
+mv txttool $HOME
+cd  $HOME /txt
+python2 install.py
+python2 update.py
+alat tx
+fi
+
+jika [ $bro  = 22 ] || [ kakak = 22 ]
+kemudian
+jernih
+toilet -f miring -F gay " Jawen vxs666 " 
+pembaruan pemasangan pkg && pembaruan pemasangan pkg
+pkg instal git
+pkg instal php
+modul cd
+php sqlscan.php
+fi
+
+jika [ $bro  = 23 ] || [ $kakak  = 23 ]
+kemudian
+jernih
+toilet -f mono12 -F gay " Jawen vxs666 " 
+peningkatan yang tepat
+apt instal python2
+tepat menginstal git
+git clone https://github.com/UltimateHackers/Hash-Buster
+mv Hash-Buster $HOME
+cd  $HOME /Hash-Buster
+python2 hash.py
+fi
+
+jika [ $bro  = 24 ] || [ $kakak  = 24 ]
+kemudian
+jernih
+toilet -f standar -F gay " Jawen vxs666 " 
+echo  " \033[33;1muntuk tutorial kunjungi: "  " \033[36;1m https://youtu.be/h2I5HU3W-so "
+tidur 3
+pembaruan yang tepat
+tepat menginstal git
+tepat menginstal php
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+mv RED_HAwk  $HOME
+CD  $HOME /RED_HAWK
+chmod +x rhawk.php
+php rhawk.php
+fi
+
+jika [ $bro  = 25 ] || [ $kakak  = 25 ]
+kemudian
+jernih
+toilet -f standar -F gay " Jawen vxs666 " 
+pembaruan yang tepat
+tepat menginstal git
+tepat menginstal untuk
+git clone https://github.com/dotfighter/torshammer.git
+mv torshammer $HOME
+cd  $HOME /torshammer
+read -p " Target Pasukan==>: " target
+python2 torshammer.py -T -t target
+fi
+
+jika [ $bro  = 26 ] || [ $kakak  = 26 ]
+kemudian
+jernih
+toilet -f standar -F gay " Mr.B4J1N64N " 
+echo  " \033[33;1muntuk tutorial kunjungi: "  " \033[36;1m https://youtu.be/EskgepQSB7U "
+tidur 3
+pembaruan yang tepat
+tepat menginstal git
+git clone https://github.com/4L13199/LITEDDOS
+mv LITEDDOS $HOME
+cd  $HOME /LITEDDOS
+baca -p " Target Masuk : " target
+python2 LITEDDOS.py target 80 100
+fi
+
+jika [ $bro  = 27   || [ $kakak  = 27 ]
+kemudian
+jernih
+toilet -f standar -F gay " Jawen vxs666 " 
+pembaruan yang tepat
+tepat menginstal git
+git clone https://github.com/b3-v3r/Hunner.git
+CD
+cd T00Ls-Jawen vxs666
+cd pemburu
+chmod +x pemburu.py
+python3 pemburu.py
+fi
+
+jika [ $bro  = 28 ] || [ $kakak  = 28 ]
+kemudian
+jernih
+toilet -f standar -F gay " Jawen vxs666 " 
+pembaruan yang tepat
+tepat menginstal git
+git clone https://github.com/sqlmapproject/sqlmap
+CD
+cd T00Ls-Jawen vxs666
+cd sqlmap
+python2 sqlmap.py
+fi
+
+jika [ $bro  = 29 ] || [ $kakak  = 29 ]
+kemudian
+jernih
+toilet -f standar -F gay " Mr.B4J1N64N " 
+pembaruan yang tepat
+tepat menginstal git
+git clone https://github.com/aryanrtm/4wsectools
+CD
+cd T00Ls-Jawen vxs666
+cd 4wsectools
+alat chmod 777
+./peralatan
+fi
+
+jika [ $bro  = 30 ] || [ $kakak  = 30 ]
+kemudian
+jernih
+figlet " Jawen vxs666 "  | lolcat
+perbarui && tingkatkan
+apt instal python2 
+tepat menginstal git
+CD
+cd T00Ls-Jawen vxs666
+modul cd
+python2 lazymux.py
+fi
+
+jika [ $bro  = 31 ] || [ $kakak  = 31 ]
+kemudian
+jernih
+figlet " Jawen vxs666 "  |lolcat
+tidur 1
+echo  " \033[32;m====================Mempercepat / hasil sinyal==================== == "
+tidur 1
+echo  " \033[32;1m================================Oleh Mr.B4J1N64N====== ========================== "
+tidur 1
+gema  " "
+gema  " "
+ping 8.8.8.8 
+fi
+
+jika [ $bro  = 32 ] || [ $kakak  = 32 ]
+kemudian
+jernih
+figlet " Jawen vxs666 "  | lolcat
+tidur 1
+echo  " \033[32;m====================Mempercepat / hasil sinyal 2================== == "
+tidur 1
+echo  " \033[32;1m================================Oleh Mr.B4J1N64N====== ========================== "
+tidur 1
+gema  " "
+gema  " "
+ping -D www.google.com
+fi
+
+jika [ $bro  = 33 ] || [ $kakak  = 33 ]
+kemudian
+jernih
+figlet " Jawen vxs666 "  | lolcat
+tidur 1
+git clone https://github.com/Hood3dRob1n/BinGoo
+cd T00Ls-Jawen vxs666
+cd BinGoo
+bash bingo
+fi
+
+jika [ $bro  = 34 ] || [ $kakak  = 34 ]
+kemudian
+jernih
+figlet " Jawen vxs666 "  | lolcat
+tidur 1
+CD
+cd T00Ls-Jawen vxs666
+modul cd
+python2 facebook.py
+fi
+
+jika [ $bro  = 35 ] || [ $kakak  = 35 ]
+kemudian
+jernih
+figlet " Jawen vxs666 "  | lolcat
+tidur 1
+pembaruan tepat && peningkatan tepat
+apt instal termux-api
+tepat menginstal git
+tepat menginstal php
+git clone https://github.com/Cvar1984/Termux-A.git
+CD
+cd T00Ls-Jawen vxs666
+cd Termux-A
+php run.php
+fi
+
+jika [ $bro  = 36 ] || [ $kakak  = 36 ]
+kemudian
+jernih
+figlet " Jawen vxs666 "  | lolcat
+tidur 1
+pembaruan tepat && peningkatan tepat
+tepat menginstal ruby
+apt install lolcat
+tepat menginstal git
+git clone https://github.com/Senitopeng/GadoGado.git
+CD
+cd T00Ls-Jawen vxs666
+cd gado gado
+bash gado
+fi
+
+jika [ $bro  = 37 ] || [ $kakak  = 37 ]
+kemudian
+jernih
+figlet " Jawen vxs666 "  | lolcat
+tidur 1
+pembaruan tepat && peningkatan tepat
+apt instal python2
+tepat menginstal git
+git clone https://github.com/verlucie/termux-lazysqlmap.git
+CD
+cd T00Ls-Jawen vxs666
+cd termux-lazysqlmap
+chmod 777 install.sh
+./install.sh
+petamalassql
+fi
+
+jika [ $bro  = 38 ] || [ $kakak  = 38 ]
+kemudian
+jernih
+figlet " Jawen vxs666 "  | lolcat
+tidur 1 
+tepat menginstal git
+tepat menginstal php
+git clone https://github.com/alintamvanz/diejoubu
+CD
+cd T00Ls-Jawen vxs666
+cd diejoubu
+cd v1.2
+php diejoubu.php
+fi
+
+jika [ $bro  = 39 ] || [ $kakak  = 39 ]
+kemudian
+jernih
+figlet " Jawen vxs666 "  | lolcat
+tidur 1 
+tepat menginstal git
+tepat menginstal php
+git clone https://github.com/evait-security/weeman.git
+CD
+cd T00Ls-Jawen vxs666
+cd weeman
+python2 weeman.py
+fi
+
+jika [ $bro  = 40 ] || [ $kakak  = 40 ]
+kemudian
+jernih
+figlet " Jawen vxs666 "  | lolcat
+tidur 1 
+tepat menginstal git
+tepat menginstal bash
+pip2 menginstal bash
+git clone https://github.com/ALX-04/iesDEFACE
+CD
+cd T00Ls-Jawen vxs666
+cd iesDEFACE
+bash iesDeface.sh
+fi
+
+jika [ $bro  = 41 ] || [ $kakak  = 41 ]
+kemudian
+jernih
+figlet " Jawen vxs666"  | lolcat
+tidur 1
+git clone https://github.com/MrKeepSmile/errorcybertool.git
+CD 
+cd T00Ls-Jawen vxs666
+cd errorcybertool
+chmod 77 errorcyber
+cd errorcybertool
+./errorcyber
+fi
+
+jika [ $bro  = 0 ] || [ $kakak  = 0 ]
+kemudian
+echo  " \033[32;1mWe Are Family DARKNESS CYBER TEAM "
+tidur 1
+echo  " \033[33;1mWe Are Anonimous "
+tidur 1
+echo  "  " We Are Legion "
+tidur 1
+echo "  " Kami Tidak Untuk Memberi "
+tidur 1
+echo  "  " Kami Tidak Untuk Mendapatkan "
+tidur 1
+echo "  " Keluarga Anonimous Indonesia "
+tidur 1
+echo  "  " Harapkan Kami "
+tidur 1
+echo " \0 33[32 ; 1m Incarlah suatu hal pasti:) Suatu saat Kamu akan Mendapatkannya:) "
+tidur 1
+keluar
+fi
